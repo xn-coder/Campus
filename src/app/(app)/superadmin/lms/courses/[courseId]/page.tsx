238983f9108ca79cffe7c38a202c23be55be6f17
Prashik Likhar <prashiklikhar009@gmail.com>
2025-09-22 10:54:31 +0000

    updates

--- src/app/(app)/superadmin/lms/courses/[courseId]/page.tsx
+++ src/app/(app)/superadmin/lms/courses/[courseId]/page.tsx
@@ -23,7 +23,7 @@ import {
   updateResourceInLessonAction,
   createSignedUploadUrlAction,
   addResourceToLessonAction,
-} from '../../actions';
+} from '../actions';
 import { supabase } from '@/lib/supabaseClient';
 import { Progress } from '@/components/ui/progress';
 import { Checkbox } from '@/components/ui/checkbox';
