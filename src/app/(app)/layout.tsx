e5b10eb99e695df28ca6b29da6838f7decaf8078
Prashik Likhar <prashiklikhar009@gmail.com>
2025-09-22 04:12:10 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: useSidebar must

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,15 @@
 
 import type React from 'react';
 import SidebarWrapper from '@/components/layout/sidebar-wrapper';
-import AppLayout from '@/components/layout/app-layout';
+import ClientLayout from './client-layout';
 
 export default async function ApplicationLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SidebarWrapper />
-      <AppLayout>
+      <ClientLayout>
         {children}
-      </AppLayout>
+      </ClientLayout>
     </>
   );
 }
